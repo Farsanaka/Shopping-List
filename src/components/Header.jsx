a0947412a58df03767e307c547758e9cc29ac7f9
Karunpr1785 <karun.p@edstem.com>
2025-04-28 16:57:47 +0530

    set logged username

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,42 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 import { logoutUser } from "../services/auth";
+import { fetchUsers } from "../services/api";
+import { useState } from "react";
+import { useEffect } from "react";
+
 function NavBar() {
   const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const loggedInUser = JSON.parse(localStorage.getItem("user"));
+    if (loggedInUser) {
+      setUser(loggedInUser);
+    }
+  }, []);
+
   const handleLogout = () => {
     logoutUser();
     navigate("/login");
   };
+
   return (
     <div>
       <nav className="flex p-3 place-content-between bg-black">
         <Logo />
         <div className="flex gap-4">
-          <FontAwesomeIcon icon={faUser} className="mt-4 text-white " />
-
-          <button onClick={handleLogout} className="text-white ">
-            Logout
-          </button>
+          <div>
+            <div className="text-white ">
+              {" "}
+              Welcome, {user ? user.name : "Guest"}
+            </div>
+            <div className="flex justify-end">
+              <FontAwesomeIcon icon={faUser} className="mt-4 text-white " />
+              <button onClick={handleLogout} className="text-white pl-2 mt-3 ">
+                Logout
+              </button>
+            </div>
+          </div>
         </div>
       </nav>
     </div>
